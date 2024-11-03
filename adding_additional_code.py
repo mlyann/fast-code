ef31b1389a713c6a97044700fd4149dbd0baab59
mlyann <mingly@arizona.edu>
2024-11-02 19:47:20 -0700

    changing cosine function

--- adding_additional_code.py
+++ adding_additional_code.py
@@ -16,28 +16,25 @@ from nltk.corpus import stopwords
 import re
 import matplotlib.pyplot as plt
 
+random.seed(721)
+torch.manual_seed(721)
+np.random.seed(721)
+
+# Process each model
+results_dir = "results-adding-noise"
+if not os.path.exists(results_dir):
+    os.makedirs(results_dir)
+
 # Load necessary libraries and data
 load_dotenv()
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('punkt_tab')
 
-random.seed(721)
-torch.manual_seed(721)
-np.random.seed(721)
 
 hf_token = os.getenv("HUGGINGFACE_HUB_TOKEN")
 dataset = load_dataset("openai_humaneval")
 glove_model = api.load("glove-wiki-gigaword-100")
-
-
-
-# Process each model
-results_dir = "results-fill-missing"
-if not os.path.exists(results_dir):
-    os.makedirs(results_dir)
-
-
 
 # Prepare data
 num_samples = 164
@@ -53,19 +50,18 @@ for i in range(num_samples):
     data.append(sample_data)
 original_df = pd.DataFrame(data)
 
-
 # Masking function
-def mask_continuous_words(code, mask_ratio=0.1):
+def adding_continuous_words(code, add_ratio=0.1):
     words = code.split()
     total_words = len(words)
-    num_to_mask = max(1, int(round(total_words * mask_ratio)))
-    start_index = random.randint(0, total_words - num_to_mask)
-    for i in range(start_index, start_index + num_to_mask):
-        words[i] = ''
+    num_to_add = max(1, int(round(total_words * add_ratio)))
+    start_index = random.randint(0, total_words - 1)
+    for i in range(start_index, start_index + num_to_add):
+        words.insert(i, 'new_word')
     return ' '.join(words)
 
-masked_prompts = [mask_continuous_words(code) for code in original_df['canonical_solution']]
-original_df['masked'] = masked_prompts
+adding_prompts = [adding_continuous_words(code) for code in original_df['canonical_solution']]
+original_df['Noisy_Code'] = adding_prompts
 
 
 # List of models to evaluate
@@ -81,18 +77,20 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Function to fix code
 def fix(prompt, model, tokenizer, max_length=200):
-    prompt = f"Here is some incomplete code:\n\n```{prompt}```\n\nGive me the complete code, without any further explanation:"
+    prompt = f"Here is some code with additional noisy characters inserting into:\n\n```{prompt}```\n\nGive me the complete code, without any further explanation:"
     inputs = tokenizer(prompt, return_tensors="pt").to(device)
     input_length = inputs["input_ids"].shape[1]
     outputs = model.generate(**inputs, max_new_tokens=max_length)
     generated_text = tokenizer.decode(outputs[0][input_length:], skip_special_tokens=True)
     return generated_text
 
+
+
 for model_name in model_names:
     tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
     model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
 
-    fixed_codes = [fix(code, model, tokenizer) for code in tqdm(original_df['masked'], desc=f"Fixing code with {model_name}")]
+    fixed_codes = [fix(code, model, tokenizer) for code in tqdm(original_df['Noisy_Code'], desc=f"Fixing code with {model_name}")]
     original_df[f"Fixed Code ({model_name})"] = fixed_codes
 
     file_name = os.path.join(results_dir, f"result-{model_name.replace('/', '-')}.csv")
@@ -125,6 +123,7 @@ def calculate_max_cosine_similarity_with_sliding_window(df, column1, column2):
     std_similarity = np.std(similarities)
     return avg_similarity, std_similarity
 
+
 # Load results and compute similarity
 results = [pd.read_csv(os.path.join(results_dir, f"result-{model_name.replace('/', '-')}.csv")) for model_name in model_names]
 labels = ['Llama', 'Yi', 'Phi', 'Gemma']
@@ -141,15 +140,16 @@ ax.bar(x, scores, yerr=std_devs, capsize=5)
 ax.set_xticks(x)
 ax.set_xticklabels(labels)
 ax.set_ylabel('Cosine Similarity')
-ax.set_title('Fill Missing Code Model Performance Evaluation')
+ax.set_title('Adding Noise Performance Evaluation')
 
 # Save the figure as a PNG file
-plt.savefig("fill-missing-code-model_performance_evaluation.png")
+plt.savefig("adding_noise_model_performance_evaluation.png")
+
 
 
 scores_base, std_devs_base = [], []
 for df, model_name in zip(results, model_names):
-    avg_sim_base, std_sim_base = calculate_max_cosine_similarity_with_sliding_window(df, 'canonical_solution', "masked")
+    avg_sim_base, std_sim_base = calculate_max_cosine_similarity_with_sliding_window(df, 'canonical_solution', "Noisy_Code")
     scores_base.append(avg_sim_base)
     std_devs_base.append(std_sim_base)
 
@@ -159,7 +159,7 @@ ax.bar(x, scores_base, yerr=std_devs_base, capsize=5)
 ax.set_xticks(x)
 ax.set_xticklabels(labels)
 ax.set_ylabel('Cosine Similarity')
-ax.set_title('Fill Missing Code Baseline')
+ax.set_title('Adding Noise Baseline')
 
 # Save the figure as a PNG file
-plt.savefig("fill_missing_code_baseline.png")
+plt.savefig("adding_noise_baseline.png")
